82bbbe4485aab1b2c21796bd2d35fd38cc8d0ff6
Ismail <ismailshaikh31097@gmail.com>
2021-03-05 10:26:41 +0530

    splash activity added

---
+++ app/src/main/java/com/example/stringchat/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.stringchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+        }, 2000)
+    }
+}
