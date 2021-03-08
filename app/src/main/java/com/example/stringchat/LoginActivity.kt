97ba59be72f8baac4453fca514944c6727013c92
Ismail <ismailshaikh31097@gmail.com>
2021-03-08 09:36:12 +0530

    constraint changes

--- app/src/main/java/com/example/stringchat/LoginActivity.kt
+++ app/src/main/java/com/example/stringchat/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.stringchat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
@@ -11,6 +12,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("LoginActivityTag", "onCreate")
         setContentView(R.layout.activity_login)
 
         sign_in_button.setOnClickListener {
@@ -22,5 +24,37 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LoginActivityTag", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LoginActivityTag", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LoginActivityTag", "onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("LoginActivityTag", "onStart")
+    }
+
+    override fun onActivityReenter(resultCode: Int, data: Intent?) {
+        super.onActivityReenter(resultCode, data)
+        Log.d("LoginActivityTag", "onActivityReenter($resultCode)")
+    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LoginActivityTag", "onDestroy")
     }
 }
