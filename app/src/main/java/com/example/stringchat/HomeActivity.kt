953ad96a638246bdfee1a627b9a2f9868ee80e44
Ismail <ismailshaikh31097@gmail.com>
2021-03-11 09:20:47 +0530

    added viewpager and tab layout

--- app/src/main/java/com/example/stringchat/HomeActivity.kt
+++ app/src/main/java/com/example/stringchat/HomeActivity.kt
@@ -2,7 +2,10 @@ package com.example.stringchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -10,18 +13,38 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val navController = fragment4.findNavController()
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if(tab?.id == R.id.tab1){
+                    viewpager.currentItem = 0
+                } else if(tab?.id == R.id.tab2){
+                    viewpager.currentItem = 1
+                } else{
+                    viewpager.currentItem = 2
+                }
+            }
 
-        fragment1.setOnClickListener {
-            navController.navigate(R.id.page1Fragment)
-        }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
 
-        fragment2.setOnClickListener {
-            navController.navigate(R.id.page2Fragment)
-        }
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
 
-        fragment3.setOnClickListener {
-            navController.navigate(R.id.page3Fragment)
+        })
+        viewpager.adapter = object :FragmentStateAdapter(this){
+            override fun getItemCount(): Int {
+                return 3
+            }
+
+            // 3 -> 0, 1, 2
+            override fun createFragment(position: Int): Fragment {
+                if(position == 0){
+                    return Page1Fragment()
+                } else if(position == 1){
+                    return Page2Fragment()
+                } else{
+                    return Page3Fragment()
+                }
+            }
         }
     }
+
 }
