8fe63d950c81379223c2bf56327551ad3ac93e6f
Ismail <ismailshaikh31097@gmail.com>
2021-03-13 10:26:41 +0530

    added options menu

--- app/src/main/java/com/example/stringchat/HomeActivity.kt
+++ app/src/main/java/com/example/stringchat/HomeActivity.kt
@@ -1,16 +1,39 @@
 package com.example.stringchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
 
-        val email = intent.getStringExtra(EMAIL_KEY)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu_options, menu)
+        return true
+    }
 
-        email_value.text = email
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.search_item ->{
+
+            }
+
+            R.id.new_broadcast_item ->{
+
+            }
+
+            R.id.new_group_item ->{
+
+            }
+        }
+        return true
     }
 }
