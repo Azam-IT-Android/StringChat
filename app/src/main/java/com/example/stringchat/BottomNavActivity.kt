97f4827ee0af7a06623570b1bd46b6a0aedd2544
Ismail <ismailshaikh31097@gmail.com>
2021-03-16 09:54:59 +0530

    added bottom navigation

---
+++ app/src/main/java/com/example/stringchat/BottomNavActivity.kt
@@ -0,0 +1,20 @@
+package com.example.stringchat
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_bottom_nav.*
+
+class BottomNavActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav)
+        val navController = findNavController(R.id.nav_host_fragment)
+        nav_view.setupWithNavController(navController)
+    }
+}
