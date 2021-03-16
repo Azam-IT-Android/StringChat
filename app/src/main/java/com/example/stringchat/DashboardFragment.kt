d7ad72330ec6d099f1155e97eb864885f53cd924
Ismail <ismailshaikh31097@gmail.com>
2021-03-16 10:24:30 +0530

    bottom navigation code cleaned up

---
+++ app/src/main/java/com/example/stringchat/DashboardFragment.kt
@@ -0,0 +1,5 @@
+package com.example.stringchat
+
+import androidx.fragment.app.Fragment
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard)
---
+++ app/src/main/java/com/example/stringchat/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.stringchat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.stringchat.R
+
+class HomeFragment : Fragment(R.layout.fragment_home)
